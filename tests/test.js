368afaa4fa4d1c354665ff2d6346fcd642835e27
Harshal Rohit <rohitharshal95@gmail.com>
2020-08-22 14:41:11 +0530

    updates

--- tests/test.js
+++ tests/test.js
@@ -233,10 +233,35 @@ const kYaml = `
         message: 
           type: "string"
 `;
+
+const jsonOrgExample = {
+  "glossary": {
+      "title": "example glossary",
+  "GlossDiv": {
+          "title": "S",
+    "GlossList": {
+              "GlossEntry": {
+                  "ID": "SGML",
+        "SortAs": "SGML",
+        "GlossTerm": "Standard Generalized Markup Language",
+        "Acronym": "SGML",
+        "Abbrev": "ISO 8879:1986",
+        "GlossDef": {
+                      "para": "A meta-markup language, used to create markup languages such as DocBook.",
+          "GlossSeeAlso": ["GML", "XML"]
+                  },
+        "GlossSee": "markup"
+              }
+          }
+      }
+  }
+}
+
+
 const body = {
-    "sourceFormat": "yaml",
-    "targetFormat": "json",
-    "content": kYaml,
+    "sourceFormat": "json",
+    "targetFormat": "yaml",
+    "content": JSON.stringify(jsonOrgExample),
 }
 
 
